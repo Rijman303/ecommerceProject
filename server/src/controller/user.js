00aec00418ec99439211ce5d9fc5fd0620ee5296
Rijman303 <rijmana703@gmail.com>
2023-06-11 13:26:45 +0545

    creating bcrypt-for unseen password and token for logined user

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,8 +1,12 @@
 const User = require ('../model/user')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
-
-
-  const registerNewUser =async (req,res)=>{
+const registerNewUser =async (req,res)=>{
+  const hashPassword =await bcrypt.hash(req.body.password, saltRounds);
+  req.body.password=hashPassword
+  console.log(hashPassword)
     const data = await User.create(req.body)
     if(data) {
       res.json({
@@ -11,14 +15,20 @@ const User = require ('../model/user')
     }
   }
 
-  const loginUser =async (req,res)=>{
-    // console.log(req.body)
-    const data = await User.findOne({phoneNumber:req.body.phoneNumber})
-    if(data) {
-      res.json({
+  const loginUser  = async (req,res)=>{
+    // step 1: check if the phoneNumber/username/email exist or not
+    const data = await User.findOne ({phoneNumber: req.body.phoneNumber})
+        // step 2: check if the password is matched or not
+     const isMatched = await bcrypt.compare(req.body.password, data.password)
+    //  step 3: generate the token for the user
+      
+     if(data && isMatched ) {
+      const token = jwt.sign({ phoneNumber: req.body.phoneNumber }, process.env.SECRET_KEY ); 
+       res.json({
         isLoggedIn:true,
         msg: "Login Successfully",
-        id: data._id
+        id: data._id,
+        token:token
       })
     }else{
       res.json({
